74cf909b1675476f88ef878103832fb091834ac4
Mithilesh-create <mithileshsharma539@gmail.com>
2023-04-23 11:55:07 +0530

    Final Push

--- src/db.js
+++ src/db.js
@@ -1,15 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyAiZbyM1FXW7SoPDP7N3m36LKgTdf4BCEQ",
-//     authDomain: "realtime-rpi-data.firebaseapp.com",
-//     databaseURL: "https://realtime-rpi-data-default-rtdb.firebaseio.com",
-//     projectId: "realtime-rpi-data",
-//     storageBucket: "realtime-rpi-data.appspot.com",
-//     messagingSenderId: "948433917930",
-//     appId: "1:948433917930:web:48174073956e0a34228493"
-// };
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_apiKey,
     authDomain: process.env.REACT_APP_authDomain,
